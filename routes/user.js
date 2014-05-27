7c4b3f5d877817e8d562a9e209100e1f7cae894f
zhaoqi <qi.zhao@hedait.com>
2014-05-27 14:01:50 +0800

    增加aa测试内容

---
+++ routes/user.js
@@ -0,0 +1,7 @@
+127.0.0.1 - - [Mon, 19 May 2014 15:14:40 GMT] "GET /archive HTTP/1.1" 200 1148 "http://localhost:3000/" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
+127.0.0.1 - - [Mon, 19 May 2014 15:14:41 GMT] "GET /stylesheets/style.css HTTP/1.1" 304 - "http://localhost:3000/" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
+127.0.0.1 - - [Mon, 19 May 2014 15:14:43 GMT] "GET /tags HTTP/1.1" 200 1013 "http://localhost:3000/archive" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
+127.0.0.1 - - [Mon, 19 May 2014 15:14:43 GMT] "GET /stylesheets/style.css HTTP/1.1" 304 - "http://localhost:3000/" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
+127.0.0.1 - - [Mon, 19 May 2014 15:15:22 GMT] "GET /tags43 HTTP/1.1" 200 210 "-" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
+127.0.0.1 - - [Mon, 19 May 2014 15:15:36 GMT] "GET /tags43 HTTP/1.1" 304 - "-" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
+127.0.0.1 - - [Mon, 19 May 2014 15:15:40 GMT] "GET /tags HTTP/1.1" 200 210 "-" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
